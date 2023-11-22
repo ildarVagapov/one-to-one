c1e1e0737ec9b7ec3dc949dec9a9460c65ecd110
Ildar <ildarvagapov528@gmail.com>
2023-11-22 19:25:27 +0500

    fix

--- src/feature/InterviewsTabs/components/MyInterviews/MyInterviews.tsx
+++ src/feature/InterviewsTabs/components/MyInterviews/MyInterviews.tsx
@@ -2,13 +2,14 @@ import { useSelector } from "react-redux"
 import style from "./MyInterviews.module.scss"
 import { useGetMyInterviewsTabInfoQuery } from "feature/InterviewsTabs/api/myInterviewsTabApi"
 import { userId } from "shared/api/userIdSlice"
-import { Link, useNavigate } from "react-router-dom"
 import { Button } from "shared/components"
+import { useNavigate } from "react-router-dom"
 
 
 export const MyInterviews = () => {
 	const { data } = useGetMyInterviewsTabInfoQuery(useSelector(userId))
 	const navigate = useNavigate()
+
 	const filters = [
 		{ id: 1, title: 'Дата, время' },
 		{ id: 2, title: 'Стэк' },
@@ -18,10 +19,6 @@ export const MyInterviews = () => {
 		{ id: 7, title: 'Статус' },
 	]
 
-	const handleFeedback = () => {
-		navigate('/feed-back')
-	}
-
 	return (
 		<div className={style.interviews}>
 			<div className={style.content}>
@@ -30,7 +27,8 @@ export const MyInterviews = () => {
 						<li>{item.dateTime}</li>
 						<li>{item.technology.name}</li>
 						<li>{item.level}</li>
-						<Button type="border" text="Подробнее" disabled={item.initiatorFeedback === 'NO_WRITE'} onClick={handleFeedback} />
+						<Button type="border" text="Подробнее" disabled={item.initiatorFeedback === 'NO_WRITE'} onClick={() => navigate('/feed-back')} />
+						<li>{item.status}</li>
 					</ul>
 				))}
 			</div>
--- src/feature/InterviewsTabs/components/SearchInterviews/SearchInterviews.tsx
+++ src/feature/InterviewsTabs/components/SearchInterviews/SearchInterviews.tsx
@@ -1,24 +1,31 @@
 import { StatusButton } from "shared/components/Tabs/components/StatusButton/StatusButton"
 import { useGetSearchInterviewsQuery } from "./api/searchInterviewsApi"
 import style from './SearchInterviews.module.scss'
+import { userId } from "shared/api/userIdSlice"
+import { useSelector } from "react-redux"
+import { Suspense } from "react"
 
 export const SearchInterviews = () => {
 	const { data } = useGetSearchInterviewsQuery()
+	const id = useSelector(userId)
 
 	return (
 		<div className={style.search}>
 			{/* <TabsFilter filters={filters} /> */}
-			<div className={style.content}>
-				{data?.items.map((item, i) => (
-					<ul key={i} className={style.items}>
-						<li>{item.dateTime}</li>
-						<li>{item.technology.name}</li>
-						<li>{item.opponentId}</li>
-						<li>{item.level}</li>
-						<StatusButton text='Откликнуться' />
-					</ul>
-				))}
-			</div>
-		</div>
+			<Suspense fallback={<p>Loading...</p>}>
+				<div className={style.content}>
+					{data?.items.map((item, i) => (
+						id !== item.initiatorId &&
+						<ul key={i} className={style.items} >
+							<li>{item.dateTime}</li>
+							<li>{item.technology.name}</li>
+							<li>{item.opponentId}</li>
+							<li>{item.level}</li>
+							<StatusButton text='Откликнуться' />
+						</ul>
+					))}
+				</div >
+			</Suspense>
+		</div >
 	)
 }
