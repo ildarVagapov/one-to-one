0c1c5527ead71578c6158bfa41638357a1413046
Ildar <ildarvagapov528@gmail.com>
2023-12-06 23:16:30 +0500

    fix

--- src/pages/InterviewsWindowPage/InterviewsWindowPage.tsx
+++ src/pages/InterviewsWindowPage/InterviewsWindowPage.tsx
@@ -2,18 +2,28 @@ import { useSelector } from "react-redux"
 import { useGetMyQuestionTabInfoQuery } from "shared/api/myQuestionTabApiInfo"
 import style from './InterviewsWindowPage.module.scss'
 import { FiSearch } from "react-icons/fi"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Accordion, AccordionBody, AccordionTitle, Button } from "shared/components"
 import { selectInitiatorId } from "shared/api/initiatorIdSlice"
 import { HeaderInterviewsWindowPage } from "./components/InterviewsWindowPageHeader/InterviewsWindowPageHeader"
 import { useSendFeedbackCreateMutation } from "./api/feedbackCreateApi"
+import { IQuestion } from "shared/types/IQuestions"
 
 export const InterviewsWindowPage = () => {
+	// переделать стэйт добавления вопросов на редакс
 	const [value, setValue] = useState<string>('')
 	const [generalComment, setGegerealComment] = useState('')
 	const id = useSelector(selectInitiatorId)
 	const { data, isSuccess, isLoading, isError } = useGetMyQuestionTabInfoQuery(id)
 	const [sendFeedbackCreate] = useSendFeedbackCreateMutation()
+	const [questions, setQuestion] = useState<IQuestion[]>([])
+
+	const addQuestion = (question: IQuestion) => {
+		if (questions.some((q: any) => q.id === question.id)) {
+			return
+		}
+		setQuestion((prevQuest => [...prevQuest, question]))
+	}
 
 	const sendFeedbackHandler = () => {
 
@@ -39,6 +49,10 @@ export const InterviewsWindowPage = () => {
 		// sendFeedbackCreate(body)
 	}
 
+	useEffect(() => {
+		console.log(questions)
+	}, [questions])
+
 	return (
 		<section className={style.interview}>
 			<div className={style.searchBar}>
@@ -49,7 +63,7 @@ export const InterviewsWindowPage = () => {
 				<div className={style.searchItems}>
 					{isLoading && <p>Загрузка...</p>}
 					{isSuccess && data?.items.filter((item) => item.answer.toLowerCase().includes(value?.toLowerCase()) || item.technology?.name.toLowerCase().includes(value?.toLowerCase())).map((item, i) => (
-						<ul key={i} className={style.questionItem}>
+						<ul key={i} className={style.questionItem} onClick={() => addQuestion(item)}>
 							<li className={style.stack}>{item.technology?.name}</li>
 							<li>{item.question}</li>
 						</ul>))}
@@ -60,7 +74,7 @@ export const InterviewsWindowPage = () => {
 				<HeaderInterviewsWindowPage />
 				<div className={style.items}>
 					{isLoading && <p>Загрузка...</p>}
-					{isSuccess && data?.items.map((item, i) => (
+					{questions.length === 0 ? <p>Добавьте вопрос из списка слева</p> : questions.map((item, i) => (
 						<Accordion key={i}>
 							<AccordionTitle id={item.id}>
 								<div className={style.item}>
